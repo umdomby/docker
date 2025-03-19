c3d8ddcaab091c4058c63c2b30f70e35e7566fd1
PC1 <umdom2@gmail.com>
2025-03-19 14:44:51 +0300

    edit

--- docker-ardu-site/app/page.tsx
+++ docker-ardu-site/app/page.tsx
@@ -1,20 +1,9 @@
-import Image from "next/image";
+
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-            ARDU
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-      </footer>
+    <div>
+        11
     </div>
   );
 }
