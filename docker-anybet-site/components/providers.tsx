b85b9568482a5f1a0eaeb8589ceec378afbcdd11
PC1 <umdom2@gmail.com>
2025-04-12 13:35:15 +0300

    edit

--- docker-anybet-site/components/providers.tsx
+++ docker-anybet-site/components/providers.tsx
@@ -1,10 +1,8 @@
 'use client';
 
-import React, {ReactNode, useEffect, useState} from 'react';
+import React, {ReactNode} from 'react';
 import {Toaster} from 'react-hot-toast';
-import {SessionProvider} from 'next-auth/react';
 import NextTopLoader from 'nextjs-toploader';
-import {ThemeProvider} from "next-themes";
 
 
 interface ProvidersProps {
@@ -13,35 +11,10 @@ interface ProvidersProps {
 
 export const Providers: React.FC<ProvidersProps> = ({children}) => {
 
-
-    const [mounted, setMounted] = useState(false);
-    const [theme, setTheme] = useState<string | null>(null); // null по умолчанию
-
-    useEffect(() => {
-        setMounted(true); // Компонент смонтирован
-        const storedTheme = localStorage.getItem('theme');
-        if (storedTheme) {
-            setTheme(storedTheme); // Используем сохраненную тему
-        }
-    }, []);
-
-    if (!mounted) {
-        return null; // Не рендерим ThemeProvider до монтирования
-    }
-
     return (
         <>
             <React.StrictMode>
-                <SessionProvider>
-                    <ThemeProvider
-                        attribute="class"
-                        defaultTheme={theme || 'system'}  // system - fallback, если нет
-                        enableSystem
-                        disableTransitionOnChange
-                    >
-                    {children}
-                    </ThemeProvider>
-                </SessionProvider>
+                {children}
             </React.StrictMode>
             <Toaster/>
             <NextTopLoader/>
