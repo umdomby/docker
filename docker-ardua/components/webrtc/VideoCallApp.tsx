ffd10754cbf56ffdeeb5259ebceada5288cac8c3
PC1 <umdom2@gmail.com>
2025-04-16 01:25:32 +0300

    edit

--- docker-ardua/components/webrtc/VideoCallApp.tsx
+++ docker-ardua/components/webrtc/VideoCallApp.tsx
@@ -8,7 +8,6 @@ import { DeviceSelector } from './components/DeviceSelector';
 import { useEffect, useState } from 'react';
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { Checkbox } from "@/components/ui/checkbox";
 
 export const VideoCallApp = () => {
